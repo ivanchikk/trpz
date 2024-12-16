23d8d52bbb6d0cb661d9b92557ccca856afa73ef
ivanchikk <vanyaklochkoff@ukr.net>
2024-12-17 01:22:26 +0200

    Implemented basic structure

---
+++ src/Shell/Services/DirectoryService.cs
@@ -0,0 +1,52 @@
+using AutoMapper;
+using Shell.Models;
+using Shell.Repositories;
+using Directory = Shell.Entities.Directory;
+
+namespace Shell.Services;
+
+public class DirectoryService(IDirectoryRepository directoryRepository, IMapper mapper) : IDirectoryService
+{
+    public async Task<IEnumerable<DirectoryDto>> GetAll()
+    {
+        return mapper.Map<IEnumerable<DirectoryDto>>(await directoryRepository.GetAll());
+    }
+
+    public async Task<IEnumerable<DirectoryDto>> GetContent(string path)
+    {
+        var directories = await directoryRepository.GetAll();
+
+        return mapper.Map<IEnumerable<DirectoryDto>>(directories.Where(d => d.Path == path));
+    }
+
+    public async Task<DirectoryDto?> GetById(int id)
+    {
+        return mapper.Map<DirectoryDto>(await directoryRepository.GetById(id));
+    }
+
+    public async Task Add(DirectoryDto dto)
+    {
+        await directoryRepository.Add(mapper.Map<Directory>(dto));
+    }
+
+    public async Task Copy(DirectoryDto dto, string path)
+    {
+        var directories = await directoryRepository.GetAll();
+        if (directories.Where(d => d.Path == dto.Path).Any(d => d.Name == dto.Name))
+            throw new Exception($"Directory with name {dto.Name} already exists");
+
+        dto.Path = dto.NewPath;
+        await directoryRepository.Add(mapper.Map<Directory>(dto));
+    }
+
+    public async Task Update(DirectoryDto dto)
+    {
+        await directoryRepository.Update(mapper.Map<Directory>(dto));
+    }
+
+    public async Task Delete(int id)
+    {
+        var dto = await GetById(id);
+        await directoryRepository.Delete(mapper.Map<Directory>(dto));
+    }
+}
